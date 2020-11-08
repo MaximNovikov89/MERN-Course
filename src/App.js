618e042e97327adb209dc7543886861d6100e3a7
MaximNovikov89 <mthelast@gmail.com>
2020-11-08 09:35:45 +0200

    Shared Components,Navbar,Modal,Places added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Users from './user/pages/Users';
 
 
 
 const App = () => {
   return <Router>
-    <Switch>
-      <Route path="/" exact>
-        <Users />
-      </Route>
+    <MainNavigation />
+    <main>
+      <Switch>
 
-      <Route path="/places/new" exact>
-        <NewPlace />
-      </Route>
+        <Route path="/" exact>
+          <Users />
+        </Route>
 
-      <Redirect to="/" />
-    </Switch>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
+    </main>
   </Router>;
 }
 
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import PlaceList from '../components/PlaceList';
+
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://www.history.com/.image/t_share/MTU3ODc3NjU2NzUxNTgwODk1/this-day-in-history-05011931---empire-state-building-dedicated.jpg',
+        adress: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878531
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://www.history.com/.image/t_share/MTU3ODc3NjU2NzUxNTgwODk1/this-day-in-history-05011931---empire-state-building-dedicated.jpg',
+        adress: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878531
+        },
+        creator: 'u2'
+    }
+]
+const UserPlaces = (props) => {
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+    return (
+        <PlaceList items={loadedPlaces} />
+    )
+}
+
+export default UserPlaces
